9f9d2316b5c3f79e91ec70cb30efb79a10148fce
hellokitty <187918229+lghui12138@users.noreply.github.com>
2026-06-16 23:49:07 +0800

    Publish Round373 fluid 181103 source-semantic shell

--- modules/auth-guard.js
+++ modules/auth-guard.js
@@ -1,6 +1,6 @@
 (() => {
   const TARGET_ORIGIN = 'https://lghui-fluid-learning.pages.dev';
-  const EDGE_REFRESH = 'round372-181103-all-html-question-cards-20260616';
+  const EDGE_REFRESH = 'round373-181103-source-semantic-practice-20260616';
   const SESSION_KEYS = ['fm_session_v2', 'fm_auth_session_v2', 'fluidMechanicsUser', 'currentUser'];
 
   function sourceUrl(pathname = location.pathname) {
