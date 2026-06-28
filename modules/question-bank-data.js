6fbe96a91f3027921bbceb7aeb79a834e61226ec
hellokitty <187918229+lghui12138@users.noreply.github.com>
2026-06-29 04:31:31 +0800

    Round563 answer depth continuation public sync

--- modules/question-bank-data.js
+++ modules/question-bank-data.js
@@ -112,7 +112,7 @@ window.QuestionBankData = (function() {
             .finally(() => clearTimeout(timer));
     }
 
-    const practiceModuleVersion = 'round562-answer-depth-continuation-20260629';
+    const practiceModuleVersion = 'round563-answer-depth-continuation-20260629';
 
     function requestedFocusBankId() {
         try {
@@ -162,7 +162,7 @@ window.QuestionBankData = (function() {
         if (!bankListHasId(banks, focusId)) return false;
         if (focusId !== '181103-material-extracted') return true;
         const versionText = String(cachedData.currentEntryVersion || cachedData.version || cachedData.updatedAt || '');
-        return /round562-answer-depth-continuation-20260629/.test(versionText);
+        return /round563-answer-depth-continuation-20260629/.test(versionText);
     }
 
     function removeFailedPracticeScripts() {
