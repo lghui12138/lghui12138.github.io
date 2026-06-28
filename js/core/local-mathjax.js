913385f1d65362b6f271893c92128fd49653ecee
hellokitty <187918229+lghui12138@users.noreply.github.com>
2026-06-28 15:38:07 +0800

    Round557 MathJax cache bust public sync

--- js/core/local-mathjax.js
+++ js/core/local-mathjax.js
@@ -1,6 +1,6 @@
 (() => {
   const SCRIPT_ID = 'MathJax-script';
-  const SCRIPT_VERSION = 'round548-181103-proof-depth-upgrade-20260627';
+  const SCRIPT_VERSION = 'round557-answer-depth-ui-polish-20260628';
   const SCRIPT_SRC = `/vendor/mathjax/es5/tex-chtml-full.js?v=${SCRIPT_VERSION}`;
   const FONT_URL = '/vendor/mathjax/es5/output/chtml/fonts/woff-v2';
   const LOAD_TIMEOUT_MS = 45000;
