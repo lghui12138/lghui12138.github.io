ff3b7525d5dedf2169e9f287a01c90aead8b4081
hellokitty <187918229+lghui12138@users.noreply.github.com>
2026-08-05 23:49:07 +0800

    Publish Round884 edge shell

--- public-shell/assets/edge-shell.js
+++ public-shell/assets/edge-shell.js
@@ -1,6 +1,6 @@
 (function () {
   const SOURCE_ORIGIN = 'https://lghui-fluid-learning.pages.dev';
-  const EDGE_REFRESH = 'round883-figure-redraw-current-20260805';
+  const EDGE_REFRESH = 'round884-ordinary-figure-qa-current-20260805';
 
   const routeMap = new Map([
     ['/knowledge.html', '/modules/knowledge-detail'],
