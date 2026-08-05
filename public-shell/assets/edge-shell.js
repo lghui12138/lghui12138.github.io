5fda38ce1d5266dd05c25eb62d1d5ccf61219970
hellokitty <187918229+lghui12138@users.noreply.github.com>
2026-08-05 22:26:19 +0800

    Publish Round880 edge shell

--- public-shell/assets/edge-shell.js
+++ public-shell/assets/edge-shell.js
@@ -1,6 +1,6 @@
 (function () {
   const SOURCE_ORIGIN = 'https://lghui-fluid-learning.pages.dev';
-  const EDGE_REFRESH = 'round879-siphon-unsteady-current-20260805';
+  const EDGE_REFRESH = 'round880-culvert-roughness-current-20260805';
 
   const routeMap = new Map([
     ['/knowledge.html', '/modules/knowledge-detail'],
