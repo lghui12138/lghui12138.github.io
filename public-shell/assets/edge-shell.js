06b12a7837e96246bcfe98039dd960c702730d56
hellokitty <187918229+lghui12138@users.noreply.github.com>
2026-08-07 00:52:38 +0800

    Publish Round901 edge shell

--- public-shell/assets/edge-shell.js
+++ public-shell/assets/edge-shell.js
@@ -1,6 +1,6 @@
 (function () {
   const SOURCE_ORIGIN = 'https://lghui-fluid-learning.pages.dev';
-  const EDGE_REFRESH = 'round900-full-derivations-4-current-20260805';
+  const EDGE_REFRESH = 'round901-ocean-world-name-current-20260805';
 
   const routeMap = new Map([
     ['/knowledge.html', '/modules/knowledge-detail'],
