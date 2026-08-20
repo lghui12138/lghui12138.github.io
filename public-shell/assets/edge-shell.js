60cfdc2d2e18725d5e4d00c307ed4d4dcd552e73
hellokitty <187918229+lghui12138@users.noreply.github.com>
2026-08-21 03:34:03 +0800

    Publish Round916 multi experiment bridge

--- public-shell/assets/edge-shell.js
+++ public-shell/assets/edge-shell.js
@@ -1,6 +1,6 @@
 (function () {
   const SOURCE_ORIGIN = 'https://lghui-fluid-learning.pages.dev';
-  const EDGE_REFRESH = 'round915-experiment-bridge-a11y-current-20260821';
+  const EDGE_REFRESH = 'round916-multi-experiment-bridge-current-20260821';
 
   const routeMap = new Map([
     ['/knowledge.html', '/modules/knowledge-detail'],
