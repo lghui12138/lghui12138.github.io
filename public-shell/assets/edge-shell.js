4415eb00d1351e5264e35d8a621aa7d00601c4ad
hellokitty <187918229+lghui12138@users.noreply.github.com>
2026-08-21 02:23:47 +0800

    Publish Round909 continue learning shell

--- public-shell/assets/edge-shell.js
+++ public-shell/assets/edge-shell.js
@@ -1,6 +1,6 @@
 (function () {
   const SOURCE_ORIGIN = 'https://lghui-fluid-learning.pages.dev';
-  const EDGE_REFRESH = 'round908-home-learning-route-a11y-current-20260821';
+  const EDGE_REFRESH = 'round909-continue-learning-route-current-20260821';
 
   const routeMap = new Map([
     ['/knowledge.html', '/modules/knowledge-detail'],
